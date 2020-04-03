108727396a9a2f25f9e839c1a7888b5f64859d2d
Ruben <rubentrevino95@gmail.com>
2020-04-03 14:38:17 -0500

    adding list

--- src/src/Contacts.java
+++ src/src/Contacts.java
@@ -5,7 +5,9 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+import java.nio.file.*;
+
+import static java.nio.file.Files.*;
 
 public class Contacts {
 
@@ -13,20 +15,26 @@ public class Contacts {
         String directory = "contactsManager"; // Declares directory name
         String filename = "contacts.txt"; // Declares file name
 
-        Path dataDirectory = Paths.get(directory); // Main path
-        Path dataFile = Paths.get(directory, filename); // Main directory
+        Path dataPath = Paths.get(directory); // Main path
+        Path dataDirectory = Paths.get(directory, filename); // Main directory
 
+
+//      ESTABLISHES FILE AND DIRECTORY EXISTS
         if (Files.notExists(dataDirectory)) { // If "file name" does NOT exist within directory, create the DIRECTORY...
             Files.createDirectories(dataDirectory);
         }
-
-        if (!Files.exists(dataFile)) { // If "file name" does NOT exist, create the FILE
-            Files.createFile(dataFile);
+        if (!Files.exists(dataPath)) { // If "file name" does NOT exist, create the FILE
+            Files.createFile(dataPath);
         }
-//        Path Files.write(Path dataDirectory, List<String> lines[, StandardOpenOption option])
+        List<String> lines = new ArrayList<>();
+        Files.write(dataDirectory, lines);
+        lines.add("1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n" +
+                "Enter an option (1, 2, 3, 4 or 5):");
 
-        List<String> contactsList = Arrays.asList("Ruben", "Anthony", "Sugar");
-        Path filepath = Paths.get("contactsManager", "contacts.txt");
-        Files.write(dataDirectory, contactsList);
+
     }
 }
