2866cd2390acff080397ef595c62cd39c8f62375
Ruben <rubentrevino95@gmail.com>
2020-04-06 15:26:52 -0500

    adding functionality

--- src/src/Contacts.java
+++ src/src/Contacts.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class Contacts {
@@ -11,7 +12,7 @@ public class Contacts {
         String filename = "contacts.txt"; // Declares file name
 
         Path dataPath = Paths.get(directory); // Main path
-        Path dataDirectory = Paths.get(directory, filename); // Main directory
+        Path dataDirectory = Paths.get(directory, filename); // Main directory to file
 
 //      ESTABLISHES FILE AND DIRECTORY EXISTS
 
@@ -49,27 +50,34 @@ public class Contacts {
 //        System.out.println("ArrayList: new array list " + contactsArray);
 
         Scanner sc = new Scanner(System.in);
+        do {
 
-        System.out.println("\n\t1. View contacts.\n" +
-                "\t2. Add a new contact.\n" +
-                "\t3. Search a contact by name.\n" +
-                "\t4. Delete an existing contact.\n" +
-                "\t5. Exit.\n" + //Save to contacts.txt on Exit (and Discard changes)
-                "\t6. Save Contacts.\n" +
-                "\tEnter an option (1, 2, 3, 4, 5, or 6):");
+            System.out.println("\n\t1. View contacts.\n" +
+                    "\t2. Add a new contact.\n" +
+                    "\t3. Search a contact by name.\n" +
+                    "\t4. Delete an existing contact.\n" +
+                    "\t5. Save Contacts.\n" +
+                    "\t6. Exit.\n" + //Save to contacts.txt on Exit (and Discard changes)
+                    "\tEnter an option (1, 2, 3, 4, 5, or 6):");
 
-        int i = sc.nextInt();
+            int i = sc.nextInt();
 
-        if (i == 1) {
-            viewContacts(dataDirectory);
-        } else if (i == 2) {
-            contactsArray.add(addContact(contactsArray, dataDirectory));
-        } else if (i == 3) {
+            if (i == 1) {
+                viewContacts(dataDirectory);
+            } else if (i == 2) {
+                contactsArray.add(addContact(contactsArray, dataDirectory));
+                Files.write(dataDirectory, contactsArray, StandardOpenOption.APPEND);
+            } else if (i == 3) {
 
-        } else if (i == 6) {
-//            TODO add to save file
-        }
+            } else if (i == 4) {
+                contactsArray.remove(removeContact(contactsArray, dataDirectory));
+                Files.write(dataDirectory, contactsArray);
+            } else if (i == 5) {
+                Files.write(dataDirectory, contactsArray);
+            } else if (i == 6) {
 
+            }
+        } while (true);
     }
 
 //  END OF MAIN ---------------------------------
@@ -131,28 +139,23 @@ public class Contacts {
         Scanner input = new Scanner(System.in);
         String newEntry = input.nextLine();
         Contacts contact = new Contacts(newEntry);
-        Files.write(dataDirectory, contactsArray);
         return newEntry;
     }
 
     // OPTION 3
 
-//    public void searchContact() {
-//        System.out.println("\t\nEnter a contact to search for (e.g. name or number): ");
-//        Scanner input = new Scanner(System.in);
-//        contactsList.get("Ruben");
-//        contactsList.getOrDefault("Sorry", "No results found!");
-//        return contactsList;
-//    }
 
     // OPTION 4
 
-//    public void deleteContact() {
-//        for (int i = 0; i < contacts.length
-//        }
-//    }
+    public static String removeContact(List<String> contactsArray, Path dataDirectory) throws IOException {
+        System.out.println("Remove a name and phone number.");
+        Scanner input = new Scanner(System.in);
+        String newEntry = input.nextLine();
+        Contacts contact = new Contacts(newEntry);
+        return newEntry;
+    }
 
-    // OPTION 6
+    // OPTION 5
 
     public void saveContacts(List<String> contactsArray, Path dataDirectory) throws IOException {
         Files.write(dataDirectory, contactsArray);
